a8fa7d88d7a4057a437761cfe40fd608c24fc34a
hghy2023 <wlq2173@naver.com>
2023-07-21 01:50:03 +0800

    Modal

--- js/index.js
+++ js/index.js
@@ -6,15 +6,13 @@ function addToCalender() {
     "https://www.google.com/calendar/render?action=TEMPLATE&text=HG%26HY+Wedding+Lunch&location=Empress&dates=20230930T040000Z%2F20230930T070000Z"
   );
 }
-
 function onRsvpBtn() {
   window.open("https://forms.gle/LroMmbAS8vU8fphu6");
 }
-
 calender.addEventListener("click", addToCalender);
 rsvp.addEventListener("click", onRsvpBtn);
 
-//swiper
+// swiper
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -34,6 +32,36 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var span = document.getElementsByClassName("close")[0];
+
+let currentImg = document.querySelector(
+  ".swiper-slide-active .swiper-zoom-container img"
+);
+
+function modalfunc() {
+  var currentSrc = currentImg.getAttribute("src");
+  modal.style.display = "flex";
+  modalImg.src = currentSrc;
+}
+
+document
+  .querySelector(".swiper-slide-active")
+  .addEventListener("click", modalfunc);
+
+swiper.on("transitionEnd", function () {
+  currentImg = document.querySelector(
+    ".swiper-slide-active .swiper-zoom-container img"
+  );
+  currentImg.addEventListener("click", modalfunc);
+});
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Thumbnail
 const number = Array.from({ length: 15 }, (v, i) => i + 1);
 const bullet = document.getElementsByClassName("swiper-pagination-bullet");
 const blarray = Array.from(bullet);
